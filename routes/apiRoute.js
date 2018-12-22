ee9708e613de94fe70066944e72c4c44c5f00351
chris pierre <lilchr@yahoo.com>
2018-12-21 20:58:27 -0800

    update

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -46,9 +46,38 @@ router.post("/resetburger" , function(req,res){
     })
 
 
+    router.post("/deleteburger ", function(req,res){
+        db.burgers.destroy(
+            {where:
+                {
+                    id: req.body.id
+                }
+            
+            }).then(function(results){
+            db.burgers.findAll({}).then(function(data){
+                res.render("index", {results:data})
+            })
+            
+        })
+        
+        })
+       
 
 router.post("/addburger" , function(req,res){
+    console.log(req);
+    db.burgers.create({
+        burger_name: req.body.burger,
+        devoured: 0,
+
+    }).then(function(data)
+    {
+        db.burgers.findAll({}).then(function(data){
+            res.render("index", {results:data})
+        })
+    })
 
 })
+
+
 
 module.exports = router;
